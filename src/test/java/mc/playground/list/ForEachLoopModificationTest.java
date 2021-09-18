d9ee804131e9145fc6c4c65c4980b1b291c3ef98
mc <odopoc@gmail.com>
2021-09-19 02:24:48 +0700

    - add foreach loop modification

---
+++ src/test/java/mc/playground/list/ForEachLoopModificationTest.java
@@ -0,0 +1,40 @@
+package mc.playground.list;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.ConcurrentModificationException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ForEachLoopModificationTest {
+
+    @Test
+    void testRemoval() {
+        List<String> a = new ArrayList<>();
+        a.add("sb");
+        a.add("sb2");
+        a.add("sb3");
+        assertThrows(ConcurrentModificationException.class, () -> {
+            for (String s : a) {
+                a.remove("sb");
+                System.out.println(s);
+            }
+        });
+    }
+
+    @Test
+    void testAdd() {
+        List<String> a = new ArrayList<>();
+        a.add("sb");
+        a.add("sb2");
+        a.add("sb3");
+        assertThrows(ConcurrentModificationException.class, () -> {
+            for (String s : a) {
+                a.add(System.currentTimeMillis() + "sb");
+                System.out.println(s);
+            }
+        });
+    }
+}
